7578ea5a0407712f34dcf3eaf6154831d9133f84
Oleg Giberstein <ogiberstein@gmail.com>
2014-05-26 20:33:03 +0100

    created pages and links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def IloveMayuba
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#IloveMayuba"
-  get "about" =>"pages#about"
-  get "home" =>"pages#home"
+  get "about" =>"pages#about" #creates about path
+  get "home" =>"pages#home" #creates home path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
