3be5170ca93ce41ceaf9b73e35987faf417f62d7
Oleg Giberstein <ogiberstein@gmail.com>
2014-05-26 19:16:41 +0100

    experimenting with pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
-  root "pages#home"
+  root "pages#IloveMayuba"
   get "about" =>"pages#about"
+  get "home" =>"pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
