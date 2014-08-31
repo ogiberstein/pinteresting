7e44324dc69ec117148f32b1ef6e98ad00805f55
Oleg Giberstein <ogiberstein@gmail.com>
2014-08-31 22:06:21 +0100

    update show view, add glyphs, changed rooth path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#WelcomePage"
+  root "pins#index"
   get "about" =>"pages#about" #creates about path
-  get "home" =>"pages#home" #creates home path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
